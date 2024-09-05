9b4e2ab557005afe7b75cf467b271505b53a128c
Vasko038 <Vasko038@outlook.com>
2024-09-05 17:55:30 +0500

    ozgarishlar boldi

--- src/forms/OfficeBudgetForm.tsx
+++ src/forms/OfficeBudgetForm.tsx
@@ -1,11 +1,9 @@
 import { useNavigate } from "react-router-dom";
 import {
   Button,
-  Col,
   DatePicker,
   Form,
   Input,
-  Row,
   Select,
   Table,
   Typography,
@@ -13,6 +11,7 @@ import {
 import styled from "@emotion/styled";
 import { FaAngleLeft } from "react-icons/fa6";
 import { TableCols } from "@data/OfficeData";
+import { useForm } from "antd/es/form/Form";
 const CustomLink = styled.a`
   color: #0000ff;
   font-size: 22px;
@@ -27,6 +26,13 @@ const { Title, Text } = Typography;
 
 function OfficeBudgetForm() {
   const navigate = useNavigate();
+  const [form] = useForm();
+  function onFinish() {
+    try {
+    } catch (e) {
+      console.log(e);
+    }
+  }
   return (
     <>
       <CustomLink onClick={() => navigate(-1)}>
@@ -46,48 +52,61 @@ function OfficeBudgetForm() {
         <Title level={3}>Create Budget</Title>
         <Text>Kindly fill in the form below to create a budget</Text>
         <Form
-          labelCol={{ span: 24 }}
-          wrapperCol={{ span: 24 }}
+          form={form}
+          onFinish={onFinish}
           layout="vertical"
+          className="grid grid-cols-3 gap-3 mt-3"
         >
-          <Row gutter={20}>
-            <Col span={8}>
-              <Form.Item label="Budget number">
-                <Input size="large" placeholder="Enter item"></Input>
-              </Form.Item>
-            </Col>
-            <Col span={8}>
-              <Form.Item label="Budget description">
-                <Input size="large" placeholder="Enter description"></Input>
-              </Form.Item>
-            </Col>
-            <Col span={8}>
-              <Form.Item label="Budget amount">
-                <Input size="large" placeholder="Enter amount in $"></Input>
-              </Form.Item>
-            </Col>
-          </Row>
-          <Row gutter={20}>
-            <Col span={8}>
-              <Form.Item
-                label="DatePicker"
-                rules={[{ required: true, message: "Please input!" }]}
-              >
-                <DatePicker size="large" style={{ width: "100%" }} />
-              </Form.Item>
-            </Col>
-            <Col span={8}>
-              <Form.Item label="Reciving options">
-                <Select placeholder="Select option" size="large" />
-              </Form.Item>
-            </Col>
-            <Col span={8}></Col>
-            <Col span={8}>
-              <Button size="large" style={{ width: "100%" }} type="primary">
-                Create Budget
-              </Button>
-            </Col>
-          </Row>
+          <Form.Item
+            rules={[{ required: true, message: "Please input!" }]}
+            name="number"
+            label="Budget number"
+          >
+            <Input size="large" placeholder="Enter item"></Input>
+          </Form.Item>
+
+          <Form.Item
+            rules={[{ required: true, message: "Please input!" }]}
+            name="description"
+            label="Budget description"
+          >
+            <Input size="large" placeholder="Enter description"></Input>
+          </Form.Item>
+
+          <Form.Item
+            name="amaount"
+            rules={[{ required: true, message: "Please input!" }]}
+            label="Budget amount"
+          >
+            <Input size="large" placeholder="Enter amount in $"></Input>
+          </Form.Item>
+
+          <Form.Item
+            name="date"
+            label="DatePicker"
+            rules={[{ required: true, message: "Please input!" }]}
+          >
+            <DatePicker size="large" style={{ width: "100%" }} />
+          </Form.Item>
+
+          <Form.Item
+            rules={[{ required: true, message: "Please input!" }]}
+            name={"recivingOptions"}
+            label="Reciving options"
+          >
+            <Select placeholder="Select option" size="large" />
+          </Form.Item>
+
+          <div />
+
+          <Button
+            size="large"
+            onClick={() => form.submit()}
+            style={{ width: "100%" }}
+            type="primary"
+          >
+            Create Budget
+          </Button>
         </Form>
       </div>
       <div
--- src/pages/admin/Circulars.tsx
+++ src/pages/admin/Circulars.tsx
@@ -1,5 +1,20 @@
+import { Flex, Input } from "antd";
+
 function Circulars() {
-  return <div>Circulars</div>;
+  return (
+    <>
+      <Flex>
+        <div style={{ width: "350px" }}>
+          <p className="mb-3 text-lg">Quick search a staff</p>
+          <Input
+            placeholder="Enter a search word"
+            className="h-[50px]"
+            style={{ borderRadius: "10px" }}
+          />
+        </div>
+      </Flex>
+    </>
+  );
 }
 
 export default Circulars;
